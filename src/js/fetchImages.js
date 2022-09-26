a6c50dfb3b1d5a1c2c54f5af6bf63146c90a9319
Tonya-Shyva <toshash69@gmail.com>
2022-09-26 21:32:26 +0300

    installed notiflix, simplelightbox. Created fetchImages.js

---
+++ src/js/fetchImages.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '30150514-c6c2592e7290a81c416aa6291';
+
+export const fetchImages = async (inputValue, pageNr) => {
+  return await fetch(
+    `&{BASE_URL}/?key=&{API_KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+  )
+    .then(async response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return [];
+        }
+        throw new Error(response.status);
+      }
+      return await response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import { fetchImages } from './js/fetchImages';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
