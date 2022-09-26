6e167c3ca55cd67790f3b18f974edc954dd166e4
Tonya-Shyva <toshash69@gmail.com>
2022-09-26 22:26:12 +0300

    almost,
    need to correct notiflix success

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -3,7 +3,7 @@ const API_KEY = '30150514-c6c2592e7290a81c416aa6291';
 
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `&{BASE_URL}/?key=&{API_KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `${BASE_URL}/?key=${API_KEY}&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
